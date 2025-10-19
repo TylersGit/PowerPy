10a3e0d2dceba33a524ad3b38ac01de3824a750e
Tyler Jordan <1997.tylerjordan@gmail.com>
2025-10-19 13:05:41 +0000

    Added helper functions for getting the last record from a stream, and for creating PSCredential objects

--- src/PowerPy/helpers.py
+++ src/PowerPy/helpers.py
@@ -1,4 +1,11 @@
+from logging import getLogger
+from System.Security import SecureString
+from System.Management.Automation import PSCredential
+
+logger = getLogger("PowerPy.helpers")
+
 _class_cache = {}
+
 
 def psobject_to_python(psobj):
     """Convert a PowerShell PSObject to a Python object with dynamic attributes."""
@@ -27,3 +34,25 @@ def psobject_to_python(psobj):
         setattr(obj, prop.Name, val)
 
     return obj
+
+def get_last_stream_record(ps_object, stream_name):
+    """Retrieve the last record from a specified PowerShell stream (e.g., Error, Warning)."""
+    valid_stream_names = ["Error", "Warning", "Verbose", "Debug", "Information", "Progress"]
+
+    if stream_name not in valid_stream_names:
+        raise ValueError(f"Invalid stream name: {stream_name}. Must be one of {valid_stream_names}.")
+
+    stream = getattr(ps_object.Streams, stream_name, None)
+    logger.debug(f"Retrieving last record from {stream_name} stream.")
+    if stream and stream.Count > 0:
+        return psobject_to_python(stream[stream.Count - 1])
+    
+    
+    
+def create_pscredential(username, password):
+    """Create a PSCredential object from a username and password."""
+    secure_password = SecureString()
+    for char in password:
+        secure_password.AppendChar(char)
+    secure_password.MakeReadOnly()
+    return PSCredential(username, secure_password)
